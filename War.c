27f01cc68b1d72760f992bb14ed57a710f5c4217
Leandro Gonçalves <167380239+LeandroG0n@users.noreply.github.com>
2025-09-01 22:22:26 +0000

    Criação de territórios.

--- War.c
+++ War.c
@@ -1,5 +1,36 @@
 #include <stdio.h>
+#include <string.h>
+
+struct Territorio{
+char nome[30];
+char cor[10];
+int tropas;
+};
+
 int main(){
-    printf("Bem vindo ao jogo War!\n");
-    return 0;
+printf("Bem vindo ao jogo War, estruturado em dados.\n");
+
+// Recebimentos dos dados de cada território
+struct Territorio territorios[5];
+for(int i = 0; i <5; i++){
+
+printf("Cadastro do território:\n");
+
+printf("Digite o nome do território:\n");
+scanf("%s", territorios[i].nome);
+
+printf("Digite a cor deste território:\n");
+scanf("%s", territorios[i].cor);
+
+printf("Digite o número de tropas deste território:\n");
+scanf("%d", &territorios[i].tropas);
 }
+
+//Exibindo dados recebidos
+for(int i=0;i < 5; i++){
+printf("Nome: %s \n Cor: %s \n Tropas: %d\n", territorios[i].nome, territorios[i].cor, territorios[i].tropas);
+}
+
+return 0;
+}
+
